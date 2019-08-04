1c23cf7c8e89599c5daa1e205a39e9c3b074e635
Ashish Kumar <ashishkumar.ashishkumar.58@gmail.com>
2019-08-04 15:31:40 +0530

    copying the IAM file outside folder

---
+++ user.tf
@@ -0,0 +1,4 @@
+resource "aws_iam_user" "one" {
+  name = "one"
+  path = "/"
+}
---
+++ variable.tf
@@ -0,0 +1,10 @@
+variable "user_name"{
+  type = "string"
+  default = "one"
+}
+
+variable "user_tag"{
+  type = "string"
+  default = "one"
+}
+
